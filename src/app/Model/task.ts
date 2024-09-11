823ffd6153e62cc316fc03e9dadff0752a399b72
metachemist <meta.chemist.22@gmail.com>
2024-09-12 02:55:48 +0500

    Adding Model Folder

---
+++ src/app/Model/task.ts
@@ -0,0 +1,41 @@
+export interface ITask{
+  itemId: number
+  taskName: string
+  taskDescription: string
+  dueDate: Date
+  createdOn: Date
+  isCompleted: boolean
+  tags: string
+  completedOn: Date
+}
+
+export class Task{
+  itemId: number
+  taskName: string
+  taskDescription: string
+  dueDate: Date
+  createdOn: Date
+  isCompleted: boolean
+  tags: string
+  completedOn: Date
+
+  constructor(){
+    this.itemId =0
+    this.taskDescription=''
+    this.completedOn=new Date()
+    this.taskName=''
+    this.tags=''
+    this.dueDate= new Date()
+    this.createdOn= new Date()
+    this.isCompleted=false
+
+  }
+}
+
+export interface ApiResponseModel{
+  message: string
+  result: string
+  data: any
+
+}
+
